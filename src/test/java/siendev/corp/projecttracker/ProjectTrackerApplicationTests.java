be531fe176edd2f1d57baa97617485a423c070aa
Iurii Golubnichenko <Iurii.Golubnichenko@gmail.com>
2024-10-24 19:21:15 +0300

    init project

---
+++ src/test/java/siendev/corp/projecttracker/ProjectTrackerApplicationTests.java
@@ -0,0 +1,11 @@
+package siendev.corp.projecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
